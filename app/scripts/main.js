5c7916cb26f23bbbfd120e1f7b076a0403d810ec
David Monson <david@808loud.com>
2016-07-02 11:55:40 -0500

    working navbar-toggler

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,13 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-$('.nav-main a.toggle').on('click', function(e) {
-	$('.nav-main nav').toggleClass('expanded');
-	$('.nav-main a.toggle > span').toggleClass('hidden');
+$('.nav-main .navbar-toggler').on('click', function(e) {
+	$('.nav-main .navbar-toggler > span').toggleClass('hidden-xs-up');
   e.preventDefault();
 });
 
-$('.nav-main ul li a').not('.nav-main a.toggle').on('click', function() {
-	$('.nav-main nav').removeClass('expanded');
+$('.nav-main ul li a').not('.nav-main .navbar-toggler').on('click', function() {
   // this needs to explicitly remove and add (rather than toggle)
   // so that a click on .home doesn't change .icon to fa-close
   // $('.nav-main ul > li > a.toggle > span').removeClass('fa-close').addClass('fa-bars');
-  $('.nav-main a.toggle > span.icon-hamburger').removeClass('hidden');
-  $('.nav-main a.toggle > span.icon-close').addClass('hidden');
+  $('.nav-main .navbar-toggler .icon-hamburger').removeClass('hidden-xs-up');
+  $('.nav-main .navbar-toggler .icon-close').addClass('hidden-xs-up');
 });
