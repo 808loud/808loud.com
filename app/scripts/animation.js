0787e4f27d527a1a8613ec1fdb069f7635e70707
David Monson <david@808loud.com>
2016-07-08 06:08:46 -0500

    major overhaul

---
+++ app/scripts/animation.js
@@ -0,0 +1,24 @@
+var $window           = $(window);
+var win_height_padded = $window.height() * 1.1;
+var isTouch           = Modernizr.touch;
+
+$window.on('scroll', animateOnScroll);
+
+function animateOnScroll() {
+  var scrolled = $window.scrollTop();
+  $(".animated-scroll:not(.animated)").each(function () {
+    var $this     = $(this);
+    var offsetTop = $this.offset().top;
+
+    if (scrolled + win_height_padded > offsetTop) {
+      if ($this.data('timeout')) {
+        window.setTimeout(function(){
+          $this.addClass('animated ' + $this.data('animation'));
+        },
+        parseInt($this.data('timeout'),10));
+      } else {
+        $this.addClass('animated ' + $this.data('animation'));
+      }
+    }
+  });
+};
